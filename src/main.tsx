5ca4e3805403ddbb86cdce42555a120b46af8115
Rafael Oliveira Pinheiro <rafael_oliveira05@hotmail.com.br>
2021-09-28 12:18:41 -0300

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div className="mx-3">
+      <p>Ola</p>
+    </div>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
